61c146d7e00784a57d4d3b2d5dfc30479a02fbd3
Kangjik Kim <rkqqkb@gmail.com>
2023-05-23 17:39:10 +0900

    members 테이블의 총 튜플을 비교하는 testJpa2

--- BE/kjkim-spring/src/test/java/kjkim/kjkimspring/repository/SignUpRepositoryTest.java
+++ BE/kjkim-spring/src/test/java/kjkim/kjkimspring/repository/SignUpRepositoryTest.java
@@ -2,9 +2,12 @@ package kjkim.kjkimspring.repository;
 
 import kjkim.kjkimspring.members.SignUp;
 import kjkim.kjkimspring.members.SignUpRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 public class SignUpRepositoryTest {
@@ -20,5 +23,15 @@ public class SignUpRepositoryTest {
         user1.setName("username");
         user1.setPhoneNumber("12312341234");
         this.signUpRepository.save(user1);
+    }
+
+    @Test
+    void testJpa_2() {
+        List<SignUp> all = this.signUpRepository.findAll();
+        Assertions.assertEquals(3, all.size());
+
+        SignUp user = all.get(0);
+        Assertions.assertEquals("username", user.getName());
+//        Assertions.assertEquals("에러가 발생하나요?!", user.getName());
     }
 }
