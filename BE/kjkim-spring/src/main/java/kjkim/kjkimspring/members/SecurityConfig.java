ad3534c39d07b3587bc67c12abe7f1cda2c33053
Kangjik Kim <rkqqkb@gmail.com>
2023-05-24 15:31:08 +0900

    csrf 토큰 설정

---
+++ BE/kjkim-spring/src/main/java/kjkim/kjkimspring/members/SecurityConfig.java
@@ -0,0 +1,17 @@
+package kjkim.kjkimspring.members;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests().antMatchers("/**").permitAll().and().csrf();
+        return http.build();
+    }
+}
