ba37e8f61aa0b09af3ebbdbc3204c52f9b997c78
Kangjik Kim <rkqqkb@gmail.com>
2023-05-25 15:38:00 +0900

    회원가입시 암호화 적용

--- BE/kjkim-spring/src/main/java/kjkim/kjkimspring/service/UserService.java
+++ BE/kjkim-spring/src/main/java/kjkim/kjkimspring/service/UserService.java
@@ -3,21 +3,23 @@ package kjkim.kjkimspring.service;
 import kjkim.kjkimspring.members.SignUp;
 import kjkim.kjkimspring.members.SignUpRepository;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private final SignUpRepository signUpRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(SignUpRepository signUpRepository) {
+    public UserService(SignUpRepository signUpRepository, PasswordEncoder passwordEncoder) {
         this.signUpRepository = signUpRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public SignUp create(String name, String email, String password) {
         SignUp user = new SignUp();
         user.setName(name);
         user.setEmail(email);
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         user.setPasswordHash(passwordEncoder.encode(password));
         this.signUpRepository.save(user);
         return user;
