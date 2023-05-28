f0418dfa1603fa43905f2591b523a2c3643c4450
Kangjik Kim <rkqqkb@gmail.com>
2023-05-28 17:45:12 +0900

    id 중복값 오류 catch

--- BE/kjkim-spring/src/main/java/kjkim/kjkimspring/controller/UserController.java
+++ BE/kjkim-spring/src/main/java/kjkim/kjkimspring/controller/UserController.java
@@ -3,6 +3,7 @@ package kjkim.kjkimspring.controller;
 import kjkim.kjkimspring.user.UserCreateForm;
 import kjkim.kjkimspring.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,9 +34,19 @@ public class UserController {
                     "2개의 패스워드가 일치하지 않습니다.");
             return "signup_form";
         }
+        try {
+            userService.create(userCreateForm.getUsername(),
+                    userCreateForm.getEmail(), userCreateForm.getPassword1());
+        } catch (DataIntegrityViolationException e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
+            return "signup_form";
+        } catch (Exception e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", e.getMessage());
+            return "signup_form";
+        }
 
-        userService.create(userCreateForm.getUsername(),
-                userCreateForm.getEmail(), userCreateForm.getPassword1());
 
         return "redirect:/";
     }
