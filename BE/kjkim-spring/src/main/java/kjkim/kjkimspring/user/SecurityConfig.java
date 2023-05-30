57542188edf29d55499eb2d9957bd21ec6637115
Kangjik Kim <rkqqkb@gmail.com>
2023-05-30 12:37:34 +0900

    로그인 db 비교기능 완료
    잘못된 id 비밀번호를 입력하면 오류 출력,
    정상적으로 로그인하면 메인화면으로 이동

--- BE/kjkim-spring/src/main/java/kjkim/kjkimspring/user/SecurityConfig.java
+++ BE/kjkim-spring/src/main/java/kjkim/kjkimspring/user/SecurityConfig.java
@@ -2,6 +2,8 @@ package kjkim.kjkimspring.user;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -24,5 +26,10 @@ public class SecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
