4cce18731878be055a87a8ebabe3bdb829fce729
Kangjik Kim <rkqqkb@gmail.com>
2023-05-31 12:45:39 +0900

    판매글 구매글 test

--- BE/kjkim-spring/src/main/java/kjkim/kjkimspring/sell/SellRepository.java
+++ BE/kjkim-spring/src/main/java/kjkim/kjkimspring/sell/SellRepository.java
@@ -3,5 +3,5 @@ package kjkim.kjkimspring.sell;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SellRepository extends JpaRepository<Sell, Integer> {
-
+    Sell findBySubject(String subject);
 }
