bfbc56b3ef959e7457f0095efe987e36290d47d1
Kangjik Kim <rkqqkb@gmail.com>
2023-06-03 21:54:14 +0900

    페이지 기능 적용

--- BE/kjkim-spring/src/test/java/kjkim/kjkimspring/repository/SellAndBuyRepositoryTest.java
+++ BE/kjkim-spring/src/test/java/kjkim/kjkimspring/repository/SellAndBuyRepositoryTest.java
@@ -109,4 +109,15 @@ public class SellAndBuyRepositoryTest {
             this.sellRepository.save(s);
         }
     }
+
+    @Test
+    void testJpa_10() {
+        for (int i = 101; i <= 200; i++) {
+            Sell s = new Sell();
+            s.setSubject(String.format("테스트 판매글 [%03d]", i));
+            s.setContent("내용 음슴");
+            s.setPrice(5000);
+            this.sellRepository.save(s);
+        }
+    }
 }
