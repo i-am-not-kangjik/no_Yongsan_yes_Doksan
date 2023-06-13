a1027de7f317dcf382b8be1db9badbfbf30348c3
Kangjik Kim <rkqqkb@gmail.com>
2023-06-13 16:07:46 +0900

    sell dto 설정

---
+++ BE/kjkim-spring/src/main/java/kjkim/kjkimspring/dto/SellDTO.java
@@ -0,0 +1,30 @@
+package kjkim.kjkimspring.dto;
+
+import kjkim.kjkimspring.sell.Sell;
+import kjkim.kjkimspring.user.User;
+import kjkim.kjkimspring.sell.SellState;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Getter
+@Setter
+public class SellDTO {
+    private Integer id;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private String imgName;
+    private String imgPath;
+    private Integer price;
+    private String authorUsername; // Author's username
+    private Integer viewCount;
+    private String region;
+    private Set<String> likedUsernames; // Set of usernames who liked the sell
+    private String category;
+    private SellState sellState;
+}
