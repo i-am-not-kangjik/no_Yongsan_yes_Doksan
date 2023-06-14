4f59a5804f176e78d8336afba3b03a0b2b2c26b1
Leebyuongho <leeno0404@naver.com>
2023-06-14 22:51:57 +0900

    commit before merge

--- FE/shop/src/pages/signin.js
+++ FE/shop/src/pages/signin.js
@@ -5,89 +5,97 @@ import { Button } from 'react-bootstrap';
 import axios from 'axios';
 
 const LoginPage = (props) => {
-  let navigate = useNavigate();
+let navigate = useNavigate();
 
-  const [id, setId] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
+const [email, setEmail] = useState('');
+const [password, setPassword] = useState('');
+const [error, setError] = useState(false);
+const [errorMessage, setErrorMessage] = useState('');
 
-  const handleIdChange = (e) => {
-    setId(e.target.value);
-  };
+const handleEmailChange = (e) => {
+setEmail(e.target.value);
+};
 
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
+const handlePasswordChange = (e) => {
+setPassword(e.target.value);
+};
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // send login request
+const handleSubmit = (e) => {
+  e.preventDefault();
 
-    axios
-      .post('http://localhost:8081/api/user/login', {
-        username: id, // 아이디를 "username"으로 변경
-        password1: password,
-      })
-      .then((response) => {
-        // 로그인 성공 시 처리
-        const token = response.data.token;
+  // 로그인 요청 보내기
+  axios
+    .post('http://localhost:8081/api/user/login', {
+      email: email, // "email" 필드를 사용합니다.
+      password: password,
+    })
+    .then((response) => {
+      // 로그인 성공 시 처리
+      const token = response.data;
 
-        // 토큰을 로컬 스토리지에 저장
+      if (token) {
+        console.log(token);
+
+        // 토큰을 로컬 스토리지에 저장합니다.
         localStorage.setItem('token', token);
 
         // 원하는 페이지로 리다이렉트
         window.location.href = '/sell';
-      })
-      .catch((error) => {
-        // 로그인 에러 처리
-        console.log(error);
+      } else {
+        // 토큰이 유효하지 않은 경우나 응답에 오류가 있는 경우 처리
         setError(true);
-        setErrorMessage('아이디나 비밀번호가 잘못 입력되었습니다');
-      });
-  };
+        setErrorMessage('에러: 유효하지 않은 토큰 또는 응답입니다.');
+      }
+    })
+    .catch((error) => {
+      // 로그인 에러 처리
+      console.log(error);
+      setError(true);
+      setErrorMessage('에러: 잘못된 이메일 또는 비밀번호입니다.');
+    });
+};
 
-  return (
-    <div className='login_box'>
-      <h1 style={{ marginBottom: '50px' }}>로그인</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor='id'></label>
-          <input
-            type='text'
-            id='id'
-            placeholder='아이디를 입력해주세요'
-            className='login_input'
-            value={id}
-            onChange={handleIdChange}
-          />
-        </div>
-        <div>
-          <label htmlFor='password'></label>
-          <input
-            type='password'
-            id='password'
-            placeholder='비밀번호를 입력해주세요'
-            className='login_input'
-            value={password}
-            onChange={handlePasswordChange}
-          />
-        </div>
-        {error && <p style={{ color: 'orange' }}>{errorMessage}</p>}
-        <Button type='submit' className='login_btn'>
-          로그인
-        </Button>
-      </form>
-      <div style={{ width: '60%', margin: 'auto' }}>
-        <Link to='/forgot-password' style={{ color: 'gray', borderRight: '1px solid gray', padding: '0 10px' }}>
-          아이디 찾기
-        </Link>
-        <Link to='/signup' className='Link' style={{ color: 'gray', padding: '0 10px' }}>
-          회원 가입
-        </Link>
-      </div>
-    </div>
-  );
+return (
+<div className='login_box'>
+<h1 style={{ marginBottom: '50px' }}>로그인</h1>
+<form onSubmit={handleSubmit}>
+<div>
+<label htmlFor='email'></label>
+<input
+         type='email'
+         id='email'
+         placeholder='이메일을 입력해주세요'
+         className='login_input'
+         value={email}
+         onChange={handleEmailChange}
+       />
+</div>
+<div>
+<label htmlFor='password'></label>
+<input
+         type='password'
+         id='password'
+         placeholder='비밀번호를 입력해주세요'
+         className='login_input'
+         value={password}
+         onChange={handlePasswordChange}
+       />
+</div>
+{error && <p style={{ color: 'orange' }}>{errorMessage}</p>}
+<Button type='submit' className='login_btn'>
+로그인
+</Button>
+</form>
+<div style={{ width: '60%', margin: 'auto' }}>
+<Link to='/forgot-password' style={{ color: 'gray', borderRight: '1px solid gray', padding: '0 10px' }}>
+이메일 찾기
+</Link>
+<Link to='/signup' className='Link' style={{ color: 'gray', padding: '0 10px' }}>
+회원 가입
+</Link>
+</div>
+</div>
+);
 };
 
 export default LoginPage;
--- FE/shop/src/pages/signup.js
+++ FE/shop/src/pages/signup.js
@@ -1,71 +1,88 @@
 import React, { useState } from 'react';
 import { Button } from 'react-bootstrap';
+import axios from 'axios';
 
 const SignupPage = () => {
-    const [name, setName] = useState('');
-    const [id, setId] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [mobileNumber, setMobileNumber] = useState('');
-    const [email, setEmail] = useState('');
-    const [passwordMatchError, setPasswordMatchError] = useState(false);
-  
-    const handleNameChange = (e) => {
-      setName(e.target.value);
-    };
-  
-    const handleIdChange = (e) => {
-      setId(e.target.value);
-    };
-  
-    const handlePasswordChange = (e) => {
-      setPassword(e.target.value);
-    };
-  
-    const handleConfirmPasswordChange = (e) => {
-      setConfirmPassword(e.target.value);
-      setPasswordMatchError(e.target.value !== password);
-    };
-  
-    const handleMobileNumberChange = (e) => {
-      const inputValue = e.target.value;
-      const numericValue = inputValue.replace(/[^0-9]/g, '');
-      const formattedValue = numericValue
-        .slice(0, 11)
-        .replace(/(\d{3})(\d{1,4})(\d{1,4})/, '$1-$2-$3');
-  
-      setMobileNumber(formattedValue);
-    };
-  
-    const handleEmailChange = (e) => {
-      setEmail(e.target.value);
-    };
-  
-    const handleSubmit = (e) => {
-      e.preventDefault();
-  
-      if (password !== confirmPassword) {
-        setPasswordMatchError(true);
-        return;
-      }
-  
-      // 회원가입 로직 실행
-      // ...
-  
-      // 폼 초기화
-      setName('');
-      setId('');
-      setPassword('');
-      setConfirmPassword('');
-      setMobileNumber('');
-      setEmail('');
-      setPasswordMatchError(false);
-    };
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  // const [mobileNumber, setMobileNumber] = useState('');
+  const [passwordMatchError, setPasswordMatchError] = useState(false);
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleConfirmPasswordChange = (e) => {
+    setConfirmPassword(e.target.value);
+    setPasswordMatchError(e.target.value !== password);
+  };
+
+  // const handleMobileNumberChange = (e) => {
+  //   const inputValue = e.target.value;
+  //   const numericValue = inputValue.replace(/[^0-9]/g, '');
+  //   const formattedValue = numericValue
+  //     .slice(0, 11)
+  //     .replace(/(\d{3})(\d{1,4})(\d{1,4})/, '$1-$2-$3');
+
+  //   setMobileNumber(formattedValue);
+  // };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
+      setPasswordMatchError(true);
+      return;
+    }
+
+    // 회원가입 요청 보내기
+    axios
+      .post('http://localhost:8081/api/user/signup', {
+        username: name,
+        email: email,
+        password1: password,
+        password2: confirmPassword,
+      })
+      .then((response) => {
+        // 회원가입 성공 시 처리
+        console.log(response);
+
+        // 폼 초기화
+        setName('');
+        setPassword('');
+        setConfirmPassword('');
+        // setMobileNumber('');
+        setEmail('');
+        setPasswordMatchError(false);
+      })
+      .catch((error) => {
+        // 회원가입 에러 처리
+        console.log(error);
+        console.log(name);
+        console.log(email);
+        console.log(password);
+        console.log(confirmPassword);
+
+        setError(true);
+        setErrorMessage('Error: Failed to sign up. Please check your input data.');
+      });
+  };
 
   return (
     <div className="login_box">
-      <h1 style={{ marginBottom : '50px' }}>회원가입</h1>
-      {/* 이름 입력란 */}
+      <h1 style={{ marginBottom: '50px' }}>회원가입</h1>
       <div>
         <label htmlFor="name"></label>
         <input
@@ -77,56 +94,6 @@ const SignupPage = () => {
           onChange={handleNameChange}
         />
       </div>
-      {/* 아이디 입력란 */}
-      <div>
-        <label htmlFor="id"></label>
-        <input
-          type="text"
-          id="id"
-          className="login_input"
-          placeholder="아이디"
-          value={id}
-          onChange={handleIdChange}
-        />
-      </div>
-      {/* 비밀번호 입력란 */}
-      <div>
-        <label htmlFor="password"></label>
-        <input
-          type="password"
-          id="password"
-          className="login_input"
-          placeholder="비밀번호"
-          value={password}
-          onChange={handlePasswordChange}
-        />
-      </div>
-      {/* 비밀번호 확인 입력란 */}
-      <div>
-        <label htmlFor="confirmPassword"></label>
-        <input
-          type="password"
-          id="confirmPassword"
-          className="login_input"
-          placeholder="비밀번호 확인"
-          value={confirmPassword}
-          onChange={handleConfirmPasswordChange}
-        />
-        {passwordMatchError && <p className="error" style={{ color : 'orange' }}>비밀번호가 일치하지 않습니다.</p>}
-      </div>
-      {/* 휴대폰 번호 입력란 */}
-      <div>
-        <label htmlFor="mobileNumber"></label>
-        <input
-          type="text"
-          id="mobileNumber"
-          className="login_input"
-          placeholder="휴대폰 번호"
-          value={mobileNumber}
-          onChange={handleMobileNumberChange}
-        />
-      </div>
-      {/* 이메일 입력란 */}
       <div>
         <label htmlFor="email"></label>
         <input
@@ -138,7 +105,45 @@ const SignupPage = () => {
           onChange={handleEmailChange}
         />
       </div>
-      {/* 가입 버튼 */}
+      <div>
+        <label htmlFor="password"></label>
+        <input
+          type="password"
+          id="password"
+          className="login_input"
+          placeholder="비밀번호"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="confirmPassword"></label>
+        <input
+          type="password"
+          id="confirmPassword"
+          className="login_input"
+          placeholder="비밀번호 확인"
+          value={confirmPassword}
+          onChange={handleConfirmPasswordChange}
+        />
+        {passwordMatchError && (
+          <p className="error" style={{ color: 'orange' }}>
+            비밀번호가 일치하지 않습니다.
+          </p>
+        )}
+      </div>
+      {/* <div>
+        <label htmlFor="mobileNumber"></label>
+        <input
+          type="text"
+          id="mobileNumber"
+          className="login_input"
+          placeholder="휴대폰 번호"
+          value={mobileNumber}
+          onChange={handleMobileNumberChange}
+        />
+      </div> */}
+      {error && <p className="error">{errorMessage}</p>}
       <Button type="submit" className="login_btn" onClick={handleSubmit}>
         회원가입
       </Button>
