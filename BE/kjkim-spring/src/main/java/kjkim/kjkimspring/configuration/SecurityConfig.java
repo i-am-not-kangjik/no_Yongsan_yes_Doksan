4271a62bbd45968fd8d7358de2f4d407c628ff30
Kangjik Kim <rkqqkb@gmail.com>
2023-06-20 20:35:11 +0900

    restapi 원칙에 맞게 securityconfig 수정

--- BE/kjkim-spring/src/main/java/kjkim/kjkimspring/configuration/SecurityConfig.java
+++ BE/kjkim-spring/src/main/java/kjkim/kjkimspring/configuration/SecurityConfig.java
@@ -6,6 +6,7 @@ import kjkim.kjkimspring.jwt.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -67,8 +68,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/api/user/signup", "/api/user/login").permitAll()
-                .antMatchers("/api/sell", "/api/sell/**").permitAll() // "/api/sell"와 "/api/sell/{id}"에 대해서 인가 없이 접근 허용
-                .antMatchers("/api/sell/create").authenticated() // "/api/sell/create"에 대해서는 인증된 사용자만 접근 가능
+                .antMatchers(HttpMethod.GET, "/api/sell", "/api/sell/**").permitAll() // "/api/sell"와 "/api/sell/{id}"에 대해서 인가 없이 접근 허용
+                .antMatchers(HttpMethod.POST, "/api/sell/**").authenticated() // "/api/sell"와 "/api/sell/{id}"에 대한 POST 요청에 대해서는 인증된 사용자만 접근 가능
+                .antMatchers(HttpMethod.PUT, "/api/sell/**").authenticated() // "/api/sell/{id}"에 대한 PUT 요청에 대해서는 인증된 사용자만 접근 가능
+                .antMatchers(HttpMethod.DELETE, "/api/sell/**").authenticated() // "/api/sell/{id}"에 대한 DELETE 요청에 대해서는 인증된 사용자만 접근 가능
                 .antMatchers("/api/**").authenticated()
                 .anyRequest().permitAll()
                 .and()
