0c5ab086f03d4f9593a006128e25af5c94927415
Kangjik Kim <rkqqkb@gmail.com>
2023-06-22 00:33:39 +0900

    configuration 주석 추가

--- BE/kjkim-spring/src/main/java/kjkim/kjkimspring/configuration/WebConfig.java
+++ BE/kjkim-spring/src/main/java/kjkim/kjkimspring/configuration/WebConfig.java
@@ -7,12 +7,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    // Cross-Origin Resource Sharing (CORS) 설정
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+        registry.addMapping("/api/**") // "/api/**" 경로에 대해 CORS 설정 적용
+                .allowedOrigins("http://localhost:3000") // 허용된 출처는 "http://localhost:3000"
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS") // 허용된 HTTP 메소드는 "GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"
+                .allowedHeaders("*") // 모든 요청 헤더 허용
+                .allowCredentials(true); // 인증정보를 포함한 요청 허용
     }
 }
